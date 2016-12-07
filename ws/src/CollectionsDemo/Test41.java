571e3979385f1b82d2dce83c3df165f659ff5b15
mrchrisbee <chris@cokuss.de>
2016-12-07 17:47:59 +0100

    jetzt inkl. Diagramme von UMLET

--- ws/src/CollectionsDemo/Test41.java
+++ ws/src/CollectionsDemo/Test41.java
@@ -2,22 +2,26 @@ package CollectionsDemo;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Vector;
 
 public class Test41 {
 
 	public static void main(String[] args) {
-		Queue<Integer> coll = new PriorityQueue<>();
-		coll.add(1);
-	//	coll.add(1,5);
-		coll.remove(0);
-	//	coll.put(1,4);
-		coll.isEmpty();
-		coll.add(1);
-		coll.peek();
-		coll.contains(2);
+		Map<Integer,Integer> coll = new HashMap<>();
+		//coll.add(1);
+		//coll.add(1,5);
+		coll.put(2,6);
+		coll.put(0,4);
+		System.out.println(coll.remove(0));
+		System.out.println(coll.isEmpty());
+		//coll.add(1);
+		//System.out.println(coll.peek());
+		//coll.contains(2);
 	}
 
 }
