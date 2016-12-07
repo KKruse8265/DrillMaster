15003b8d1285a738fa2cc729d8b8e05ca7ffdf24
mrchrisbee <chris@cokuss.de>
2016-12-07 18:02:50 +0100

    jetzt inkl. Diagramme von UMLET

--- ws/src/CollectionsDemo/Test41.java
+++ ws/src/CollectionsDemo/Test41.java
@@ -13,12 +13,12 @@ public class Test41 {
 
 	public static void main(String[] args) {
 		Map<Integer,Integer> coll = new HashMap<>();
-		//coll.add(1);
-		//coll.add(1,5);
-		coll.put(2,6);
+		//coll.add(1); // ist schon in Collection definiert
+		//coll.add(1,5);// ist schon in Collection definiert (füge an [pos] [wert] ein)
+		coll.put(2,6); //füge einer Map etwas hinzu ([key] [value])
 		coll.put(0,4);
-		System.out.println(coll.remove(0));
-		System.out.println(coll.isEmpty());
+		System.out.println(coll.remove(0)); // ist in List und in Map definiert
+		System.out.println(coll.isEmpty());// ist in Collection und in Map
 		//coll.add(1);
 		//System.out.println(coll.peek());
 		//coll.contains(2);
