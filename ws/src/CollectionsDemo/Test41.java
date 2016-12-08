ace493a4bf0724ababb6a9118e2d84d58d3f104b
KKruse8265 <Kerstin.Kruse-Wustrow@web.de>
2016-12-08 10:34:42 +0100

    Kommentare angepasst in Test41, QueusDemo erzeugt

--- ws/src/CollectionsDemo/Test41.java
+++ ws/src/CollectionsDemo/Test41.java
@@ -13,15 +13,17 @@ public class Test41 {
 
 	public static void main(String[] args) {
 		Map<Integer,Integer> coll = new HashMap<>();
-		//coll.add(1); // ist schon in Collection definiert
-		//coll.add(1,5);// ist schon in Collection definiert (fÃ¼ge an [pos] [wert] ein)
-		coll.put(2,6); //fÃ¼ge einer Map etwas hinzu ([key] [value])
-		coll.put(0,4);
-		System.out.println(coll.remove(0)); // ist in List und in Map definiert
+		//coll.add(1); // ist schon in Collection definiert -->Alle List, Set und Queue
+		//coll.add(1,5);// ist nur in List ( und Subtypen von List) definiert (fuege an [index] [wert] ein)
+		coll.put(2,6); //fuege einer Map etwas hinzu ([key] [value]) -->Map ist K e i n e Collection
+		coll.put(2,4);	//put auf einen bestehenden key überschreibt den Value für diesen Keys
+		System.out.println(coll.remove(0)); // ist in Collection und in Map definiert
 		System.out.println(coll.isEmpty());// ist in Collection und in Map
-		//coll.add(1);
-		//System.out.println(coll.peek());
-		//coll.contains(2);
+		//coll.add(1);	//Bei Sets wird k e i n Dupilkat hinzugefügt add liefert in dem Fall false
+		//System.out.println(coll.peek()); //wird in Queue verwendet
+		//coll.contains(2); // wird in allen Collection`s verwendet
+		coll.containsKey(2); // definiert in Map und ermittelt, ob der Key existiert
+		coll.containsValue(2);//definiert in Map und ermittelt, ob der Value existiert
 	}
 
 }
