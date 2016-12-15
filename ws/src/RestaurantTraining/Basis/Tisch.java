ad80a0278effed7da573b564ee1e3b2ebf074c4b
Christian Beerwerth <icke@cokuss.de>
2016-12-15 09:25:42 +0100

    nur kleine Änderungen

--- ws/src/RestaurantTraining/Basis/Tisch.java
+++ ws/src/RestaurantTraining/Basis/Tisch.java
@@ -8,15 +8,23 @@ public class Tisch {
 	private int tischnummer;
 	private boolean belegt;
 	private int plaetze;
-	// wie die Realisierung der Reservierung funzen soll ist mir noch völlig fraglich 
-	//Calendar.set(year + 1900, month, date, hrs, min)
+	// wie die Realisierung der Reservierung funzen soll ist mir noch völlig
+	// fraglich
+	// Calendar.set(year + 1900, month, date, hrs, min)
 	// Calendar rightNow = Calendar.getInstance();
-	
+
 	public Tisch(int tischnummer, boolean belegt, int plaetze) {
 		this.tischnummer = tischnummer;
 		this.belegt = belegt;
 		this.plaetze = plaetze;
 	}
 
-	
+	public String toString() {
+		String tmp;
+		if (belegt)
+			tmp = "Besetzt";
+		else
+			tmp = "Frei";
+		return "Tisch " + tischnummer + " " + tmp;
+	}
 }
