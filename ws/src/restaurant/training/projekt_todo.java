061e3bd02ff9e87006f375a92c42605e6a17c6c0
Christian Beerwerth <icke@cokuss.de>
2017-01-02 14:31:51 +0100

    inkl. neue Todos

---
+++ ws/src/restaurant/training/projekt_todo.java
@@ -0,0 +1,15 @@
+package restaurant.training;
+
+
+// TODO's Christian 
+// todos sind ne tolle Sache, vielleicht sollten wir die auch Zentral und lokal pflegen
+
+// ich fände es gut in diesem Projekt auch gleich ein paar tests einzufügen, 
+// bisher machen wir das ja eher über die main, professioneller und besser 
+// fände ich dann aber doch die unit Tests
+// von denen ich bisher auch noch keine Ahnung habe, ist aber ein super wichtiges Thema
+//und bestimmt auch keine Zauberei, macht dein Beispiel auch runder
+
+// als nächsten schritt möchte ich einen Test (bon mir aus auch erstmal in der main) der Reservierung machen
+// TODO's Kerstin
+// ...
